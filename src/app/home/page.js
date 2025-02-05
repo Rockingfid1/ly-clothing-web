a4de5b9e278dd676187deba6a1bb289059aa774f
Rockingfid1 <chilliwillizpeeps@gmail.com>
2025-02-05 00:47:55 -0500

    styling

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,8 +1,9 @@
 import { GlobalColors } from "@/lib/constants/theme/globalColors";
+import { Box, Typography } from "@mui/material";
 
 export default function HomePage() {
   return (
-    <div
+    <Box
       style={{
         height: "100vh",
         background: `linear-gradient(to bottom, ${GlobalColors.app_bg_gradient1}, ${GlobalColors.app_bg_gradient2})`,
@@ -11,7 +12,7 @@ export default function HomePage() {
         alignItems: "center",
       }}
     >
-      This is the home page
-    </div>
+      <Typography variant="h3">This is the home page</Typography>
+    </Box>
   );
 }
